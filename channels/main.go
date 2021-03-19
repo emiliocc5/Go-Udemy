bceec40da47f95f18891aefd9b59adea885d9837
emiliocc5 <emiliocaccia@live.com.ar>
2021-03-19 00:27:22 -0300

    Added channels

--- channels/main.go
+++ channels/main.go
@@ -14,16 +14,27 @@ func main() {
 		"http://amazon.com",
 	}
 
+	c := make(chan string)
+
 	for _, link := range links {
-		go checkLink(link)
+		go checkLink(link, c)
+	}
+	for i := 0; i < len(links); i++ {
+		fmt.Println(<-c)
 	}
 }
 
-func checkLink(link string) {
+func checkLink(link string, c chan string) {
 	_, err := http.Get(link)
+	m := link
 	if err != nil {
-		fmt.Println(link, " might be down")
-		return
+		//fmt.Println(link, " might be down")
+		//c <- "Might be down I think"
+		m += " might be down"
+		//return
 	}
-	fmt.Println(link, " is up!")
+	//fmt.Println(link, " is up")
+	//c <- "Yeah its up"
+	m += " is up!"
+	c <- m
 }
